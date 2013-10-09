d31841f8cd3f4c95294d58c667832aab17f83d37
popescuadrian <adrian_vasile2000@yahoo.com>
2013-10-09 14:14:43 +0300

    first commit

---
+++ src/test/java/de/jurion/requirements/Application.java
@@ -0,0 +1,11 @@
+package de.jurion.requirements;
+
+import net.thucydides.core.annotations.Feature;
+
+public class Application {
+    @Feature
+    public class Search {
+        public class SearchByKeyword {}
+        public class SearchByMultipleKeywords {}
+    }
+}

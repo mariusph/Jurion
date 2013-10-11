3edf3fad717d4640f16c519c2ff860d082ee59f8
marius.pojar <marius.pojar@HKI-L048.enoro.com>
2013-10-11 10:17:45 +0300

    Minor change in Constants.

--- src/main/java/de/jurion/tools/Constants.java
+++ src/main/java/de/jurion/tools/Constants.java
@@ -3,6 +3,6 @@ package de.jurion.tools;
 public class Constants {
 	public static final String BASE_URL = "http://www.jurion.de";
 
-	public static final String TESTDATA_FILES_PATH = "src/test/java/de.jurion/testdata/";
+	public static final String TESTDATA_FILES_PATH = "src/test/java/de/jurion/testdata/";
 	public static final char CSV_SEPARATOR = ',';
 }

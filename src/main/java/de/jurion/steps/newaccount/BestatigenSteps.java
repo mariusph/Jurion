bf1808284172f97abed8685f18c619ec7468a715
Adrian Popescu <adrian_vasile2000@yahoo.com>
2013-10-29 00:15:55 +0200

    test

--- src/main/java/de/jurion/steps/newaccount/BestatigenSteps.java
+++ src/main/java/de/jurion/steps/newaccount/BestatigenSteps.java
@@ -6,20 +6,17 @@ import net.thucydides.core.annotations.Step;
 import net.thucydides.core.pages.Pages;
 import de.jurion.tools.AbstractSteps;
 
-public class BestatigenSteps extends AbstractSteps{
+public class BestatigenSteps extends AbstractSteps {
 
-	public BestatigenSteps(Pages pages) {
-		super(pages);
-		}
+    public BestatigenSteps(Pages pages) {
+        super(pages);
+    }
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -4511021179502109708L;
-    
-	@Step
-	public List<String> verifyProfessionalList() {
-		return bestatigenPage().verifyProfessionalList();
-	}
+  private static final long serialVersionUID = -4511021179502109708L;
+
+    @Step
+    public List<String> verifyProfessionalList() {
+        return bestatigenPage().verifyProfessionalList();
+    }
 
 }

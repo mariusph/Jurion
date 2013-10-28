538ca0e4165c3c29735eeea1ced37297cc39933b
Adrian Popescu <adrian_vasile2000@yahoo.com>
2013-10-29 00:08:05 +0200

    test

--- src/test/java/de/jurion/test/CreateAccountTest.java
+++ src/test/java/de/jurion/test/CreateAccountTest.java
@@ -21,147 +21,144 @@ import de.jurion.tools.CustomerLabels;
 
 @Story(Application.User.CreateUser.class)
 @RunWith(ThucydidesParameterizedRunner.class)
-@UseTestDataFrom(value = Constants.TESTDATA_FILES_PATH + ConfigFileLibrary.CREATE_CUSTOMER,
-separator = Constants.CSV_SEPARATOR)
-
+@UseTestDataFrom(value = Constants.TESTDATA_FILES_PATH
+        + ConfigFileLibrary.CREATE_CUSTOMER, separator = Constants.CSV_SEPARATOR)
 public class CreateAccountTest extends BaseTest {
-	
+
     // private CustomerModel customerModel;
-	
-	private String institution;
-	private String firstname;
-	private String lastname;
-	private String address;
-	private String plz;
-	private String city;
-	private String email;
-	private String phone;
-	private String password;
-	
-	public void setInstitution(String institution) {
-		this.institution = institution;
-	}
-	
-	public String getInstitution() {
-		return institution;
-	}
 
+    private String institution;
+    private String firstname;
+    private String lastname;
+    private String address;
+    private String plz;
+    private String city;
+    private String email;
+    private String phone;
+    private String password;
 
-	public String getFirstname() {
-		return firstname;
-	}
+    public void setInstitution(String institution) {
+        this.institution = institution;
+    }
 
-	public String getLastname() {
-		return lastname;
-	}
+    public String getInstitution() {
+        return institution;
+    }
 
-	public String getAddress() {
-		return address;
-	}
+    public String getFirstname() {
+        return firstname;
+    }
 
-	public String getPlz() {
-		return plz;
-	}
+    public String getLastname() {
+        return lastname;
+    }
 
-	public void setFirstname(String firstname) {
-		this.firstname = firstname;
-	}
+    public String getAddress() {
+        return address;
+    }
 
-	public void setLastname(String lastname) {
-		this.lastname = lastname;
-	}
+    public String getPlz() {
+        return plz;
+    }
 
-	public void setAddress(String address) {
-		this.address = address;
-	}
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
 
-	public void setPlz(String plz) {
-		this.plz = plz;
-	}
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
 
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
-	public String getCity() {
-		return city;
-	}
+    public void setPlz(String plz) {
+        this.plz = plz;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public String getCity() {
+        return city;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public String getPhone() {
-		return phone;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
+    public String getPhone() {
+        return phone;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	public void setCity(String city) {
-		this.city = city;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
+    public void setCity(String city) {
+        this.city = city;
+    }
 
-	@Steps
-	public HomeSteps homeSteps;
-	
-	@Steps
-	public RegistrierungStartenSteps registrierungStartenSteps;
-	
-	@Steps
-	public PersonlicheDatenSteps personlicheDatenSteps;
-	
-	@Steps
-	public LoginDatenSteps loginDatenSteps;
-	
-	@Steps
-	public BestatigenSteps bestatigenSteps;
+    @Steps
+    public HomeSteps homeSteps;
 
-	@Test
-	public void createNewAccount() {
-		
-		homeSteps.openHomePage();
-		
-		// click on 'Registrien' button
-		homeSteps.clickOnRegistrierenButton();
-		
-		// click on 'Weiter' button to start filling the register form
-		registrierungStartenSteps.clickOnWeiterButton();
-		
-		// fill the data
-		email= RandomStringUtils.randomAlphanumeric(5)+ email;
-		personlicheDatenSteps.fillTheDataForm(institution,
-				CustomerLabels.GENDER_MALE, CustomerLabels.ACADEMIC_PROF_DR,
-				firstname, lastname, address, plz, city, CustomerLabels.COUNTRY,
-				email, phone, CustomerLabels.PROFESSION);
-		
-		// login credentials
-		loginDatenSteps.getTheUsername();
-		String username = loginDatenSteps.getTheUsername();
-		
-		loginDatenSteps.fillPasswords(password);
-		
-		loginDatenSteps.expandDomainArrow();
-		loginDatenSteps.clickOnInsolventzRechtCheckbox();
-		loginDatenSteps.closeDomainArrow();
-		
-		loginDatenSteps.getProfessionalList();
-		loginDatenSteps.clickOnWeiterButton();
-		
-		// verify customer's info's
-		bestatigenSteps.verifyProfessionalList();
+    @Steps
+    public RegistrierungStartenSteps registrierungStartenSteps;
 
-	}
+    @Steps
+    public PersonlicheDatenSteps personlicheDatenSteps;
+
+    @Steps
+    public LoginDatenSteps loginDatenSteps;
+
+    @Steps
+    public BestatigenSteps bestatigenSteps;
+
+    @Test
+    public void createNewAccount() {
+
+        homeSteps.openHomePage();
+
+        // click on 'Registrien' button
+        homeSteps.clickOnRegistrierenButton();
+
+        // click on 'Weiter' button to start filling the register form
+        registrierungStartenSteps.clickOnWeiterButton();
+
+        // fill the data
+        email = RandomStringUtils.randomAlphanumeric(5) + email;
+        personlicheDatenSteps
+                .fillTheDataForm(institution, CustomerLabels.GENDER_MALE,
+                        CustomerLabels.ACADEMIC_PROF_DR, firstname, lastname,
+                        address, plz, city, CustomerLabels.COUNTRY, email,
+                        phone, CustomerLabels.PROFESSION);
+
+        // login credentials
+        loginDatenSteps.getTheUsername();
+        String username = loginDatenSteps.getTheUsername();
+
+        loginDatenSteps.fillPasswords(password);
+
+        loginDatenSteps.expandDomainArrow();
+        loginDatenSteps.clickOnInsolventzRechtCheckbox();
+        loginDatenSteps.closeDomainArrow();
+
+        loginDatenSteps.getProfessionalList();
+        loginDatenSteps.clickOnWeiterButton();
+
+        // verify customer's info's
+        bestatigenSteps.verifyProfessionalList();
+
+    }
 
 }

4f71b41a3e831efdc83605b21fd10da55333c376
Adrian Popescu <adrian_vasile2000@yahoo.com>
2013-12-09 16:39:02 +0200

    minor changes

--- src/test/java/de/jurion/test/BaseTest.java
+++ src/test/java/de/jurion/test/BaseTest.java
@@ -5,7 +5,10 @@ import net.thucydides.core.annotations.ManagedPages;
 import net.thucydides.core.pages.Pages;
 
 import org.junit.After;
+import org.junit.Before;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import de.jurion.tools.Constants;
 
@@ -17,6 +20,17 @@ public class BaseTest {
     @ManagedPages(defaultUrl = Constants.BASE_URL)
     public Pages pages;
 
+    @Before
+    public void first() {
+
+        
+        if(webdriver instanceof ChromeDriver) {
+            System.out.println("chrome DRIVER");
+        }else if(webdriver instanceof FirefoxDriver) {
+            System.out.println("FF DRIVER");
+        }
+       
+    }
     @After
     public void closeSelenium() {
         webdriver.close();
